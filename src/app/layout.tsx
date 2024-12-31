0c5a9aeda7bf2e75f005f0b499771977c934fc03
chebon254 <kelvinchebon90@gmail.com>
2024-12-31 14:27:40 +0300

    favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {
-  ClerkProvider,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/NavBar/Navbar";
 
 // import {
@@ -37,12 +35,29 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <link
+            rel="icon"
+            type="image/png"
+            href="/favicon/favicon-96x96.png"
+            sizes="96x96"
+          />
+          <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+          <link rel="shortcut icon" href="/favicon/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <meta name="apple-mobile-web-app-title" content="Great Rift Run" />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+        </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} relative z-0 antialiased`}
         >
           <Navbar />
           <main className="p-4 bg-[#f3f4f6] min-h-[calc(100vh-80px)]">
-          {children}
+            {children}
           </main>
         </body>
       </html>
